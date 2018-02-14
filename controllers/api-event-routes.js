0c468dc1b1e8296cce616555068682b2e064e5db
Shi-Kwan <sktan@insytecg.com>
2018-02-14 14:32:52 -0500

    added some more routes in users and events, and added session code in main.js

--- controllers/api-event-routes.js
+++ controllers/api-event-routes.js
@@ -1,18 +1,23 @@
-var db = require("../app/models");
+var db = require('../app/models')
 
-module.exports = function(app){  
-    var eventToDisplay = {};
-    app.get('/event/', function(req, res, next){
-        db.Events.findAll({}).then(function(events){
-            console.log("In event API route, ")
-            console.log(JSON.stringify(events))
-            eventToDisplay = {
-                eventTitle : "All Events",
-                eventDisplay : events,
-            };
-        })
-        res.render('index' , eventToDisplay)
-    })    
+module.exports = function (app) {
+  var eventToDisplay = {}
+  app.get('/event/:order?', function (req, res, next) {
+    var orderStatement = {}
+    
+    if (req.body.order) {
+      orderStatement.order = req.params.order // orderBy must include column name and asc/desc i.e: name ASC, gender DESC
+    }
 
-
+    db.Events.findAll(orderStatement).then(function (events) {
+      console.log('In event API route, ')
+      console.log(JSON.stringify(events))
+      eventToDisplay = {
+        eventTitle: 'All Events',
+        eventDisplay: events
+      }
+    })
+    res.render('index' , eventToDisplay)
+    
+  })
 }
--- controllers/api-user-routes.js
+++ controllers/api-user-routes.js
@@ -1,69 +1,65 @@
- var db = require("../app/models");
+var db = require('../app/models')
 
-module.exports = function(app){
-    app.get('/', function(req, res, next){
-        db.User.findAll({}).then(function(users){
-            console.log("get users from sql")
-            console.log(JSON.stringify(users));
-            db.Events.findAll({}).then(function(events){
-                console.log("get events from sql")
-                console.log(JSON.stringify(events));
-                res.render('index', 
-                    {
-                        title: 'Andale!!',
-                        users : users,
-                        events : events,
-                    }
-                )
-            })
-            
-        })
+module.exports = function (app) {
+  app.get('/', function (req, res, next) {
+    db.User.findAll({}).then(function (users) {
+      console.log('get users from sql')
+      console.log(JSON.stringify(users))
+      db.Events.findAll({}).then(function (events) {
+        console.log('get events from sql')
+        console.log(JSON.stringify(events))
+        res.render('index',
+          {
+            title: 'Andale!!',
+            users: users,
+            events: events
+          }
+        )
+      })
     })
+  })
 
-    app.get('/api/getUser/:username/:password', function(req, res, next){
-        console.log("in getUser..");
-        db.User.findAndCountAll({
-            where:{
-                username : req.body.username,
-                password : req.body.password,
-            },
-            limit : 1
-        }).then(function(credentials){
-            console.log("Result return : " + credentials.count);
-            if(credentials.count > 0 ){
-                console.log("Found user!")
-                res.json({credentials : credentials})
-                console.log(JSON.stringify(credentials));
-
-            }else {
-                res.json({error : "User not found!"})
-                next("User not found!");
-                
-            }
-        }).catch(function(err){
-            console.log(err)
-            next(err);
-        })
+  app.get('/api/getUser/:username/:password', function (req, res, next) {
+    console.log('in getUser..')
+    db.User.findAndCountAll({
+      where: {
+        username: req.params.username,
+        password: req.params.password
+      },
+      limit: 1
+    }).then(function (credentials) {
+      console.log('Result return : ' + credentials.count)
+      if (credentials.count > 0) {
+        console.log('Found user!')
+        res.send(credentials)
+        console.log(JSON.stringify(credentials))
+      }else {
+        res.json({error: 'User not found!'})
+        next('User not found!')
+      }
+    }).catch(function (err) {
+      console.log(err)
+      next(err)
     })
+  })
 
-    app.post('/api/user/', function(req, res, next){
-        console.log("first name ", first_name);
-        db.User.create({
-
-            first_name : req.body.first_name,
-            last_name : req.body.last_name,
-            gender : req.body.gender,
-            username : req.body.username,
-            email : req.body.email,
-            password : req.body.password,
-            location : req.body.location,
-            hometown : req.body.hometown,
-            dob : req.body.dob, 
-            photo : req.body.photo,
-            bio : req.body.bio
-        }).then(function(results){
-            console.log(results);
-            res.JSON(results);
-        })
+  app.post('/api/user/', function (req, res, next) {
+    console.log('first name ', first_name)
+    db.User.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      gender: req.body.gender,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      location: req.body.location,
+      hometown: req.body.hometown,
+      dob: req.body.dob,
+      photo: req.body.photo,
+      bio: req.body.bio
+    }).then(function (results) {
+      console.log(results)
+      res.JSON(results)
     })
-};
+  })
+}
