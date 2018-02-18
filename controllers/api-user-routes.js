1947935400629ac6a11b426995de23f8ab78bed9
unknown <bradwayne74@gmail.com>
2018-02-18 16:09:14 -0500

    user sport field 2 columns

--- controllers/api-user-routes.js
+++ controllers/api-user-routes.js
@@ -432,7 +432,7 @@ module.exports = function (app) {
                 }).then(function (likeEventInfo) {
                   objUser.likeEventInfo = likeEventInfo
                   // res.json(objUser)
-                  res.render('activity', objUser)
+                  res.render('user', objUser)
                 })
               })
             })
