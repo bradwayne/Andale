a1a346d680bbd3939f0f6f9341f38d8c373c2ff1
unknown <bradwayne74@gmail.com>
2018-02-19 16:10:02 -0500

    modals adjusted

--- controllers/api-user-routes.js
+++ controllers/api-user-routes.js
@@ -579,7 +579,7 @@ module.exports = function (app) {
                 }).then(function (likeEventInfo) {
                   objUser.likeEventInfo = likeEventInfo
                   // res.json(objUser)
-                  res.render('activity', objUser)
+                  res.render('home', objUser)
                 })
               })
             })
