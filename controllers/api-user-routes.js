993c459be33057a50be51c8c59de396b64e5ef44
unknown <bradwayne74@gmail.com>
2018-02-19 00:27:23 -0500

    handlebar progressing

--- controllers/api-user-routes.js
+++ controllers/api-user-routes.js
@@ -572,7 +572,7 @@ module.exports = function (app) {
                 }).then(function (likeEventInfo) {
                   objUser.likeEventInfo = likeEventInfo
                   //res.json(objUser)
-                  res.render('user', objUser)
+                  res.render('events', objUser)
                 })
               })
             })
